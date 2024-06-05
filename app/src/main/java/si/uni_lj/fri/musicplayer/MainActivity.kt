1878c3b404bde5a77275ed450839596fbcc4451b
veljko <veljko.pejovic@fri.uni-lj.si>
2024-06-05 11:48:47 +0100

    Initial commit.

--- app/src/main/java/si/uni_lj/fri/musicplayer/MainActivity.kt
+++ app/src/main/java/si/uni_lj/fri/musicplayer/MainActivity.kt
@@ -1,24 +1,14 @@
 package si.uni_lj.fri.musicplayer
 
 
-import android.app.ActivityManager
-import android.content.BroadcastReceiver
-import android.content.ComponentName
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
-import android.content.ServiceConnection
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.os.IBinder
 import android.util.Log
-import android.view.View
-import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import java.io.IOException
 import java.util.*
 
@@ -27,29 +17,8 @@ class MainActivity : AppCompatActivity() {
     private var startServiceButton: Button? = null
     private var stopServiceButton: Button? = null
     private var aboutButton: Button? = null
-    private var playButton: Button? = null
-    private var stopButton: Button? = null
 
-    private var service: MusicService? = null
-
-    private val connection: ServiceConnection = object: ServiceConnection{
-        override fun onServiceConnected(name: ComponentName?, binder: IBinder?) {
-            Log.i(TAG, "onServiceConnected()")
-            service = (binder as MusicService.LocalBinder).service
-            musicInfoTextView?.text = service?.song
-        }
-
-        override fun onServiceDisconnected(name: ComponentName?) {
-            Log.i(TAG, "onServiceDisconnected()")
-            service = null
-        }
-    }
-
-    private val receiver:BroadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            musicInfoTextView?.text = intent?.getStringExtra("song")
-        }
-    }
+    private var player: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,31 +29,22 @@ class MainActivity : AppCompatActivity() {
         musicInfoTextView = findViewById(R.id.musicInfoTextView)
         startServiceButton = findViewById(R.id.startServiceButton)
         stopServiceButton = findViewById(R.id.stopServiceButton)
-        playButton = findViewById(R.id.playButton)
-        stopButton = findViewById(R.id.stopButton)
         aboutButton = findViewById(R.id.aboutButton)
 
-        playButton?.setOnClickListener {
-            service?.play()
-        }
-
-        stopButton?.setOnClickListener {
-            service?.stop()
-        }
-
         startServiceButton?.setOnClickListener {
-            val intent = Intent(this, MusicService::class.java)
-            startService(intent)
-            bindService(intent, connection, BIND_AUTO_CREATE)
+            Toast.makeText(
+                applicationContext,
+                "Start service button will be used in the service implementation.",
+                Toast.LENGTH_SHORT
+            ).show()
         }
-
         stopServiceButton?.setOnClickListener {
-            unbindService(connection)
-            service = null
-            val intent = Intent(this, MusicService::class.java)
-            stopService(intent)
+            Toast.makeText(
+                applicationContext,
+                "Stop service button will be used in the service implementation.",
+                Toast.LENGTH_SHORT
+            ).show()
         }
-
         aboutButton?.setOnClickListener {
             startActivity(
                 Intent(
@@ -97,41 +57,65 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-
-        if (isServiceRunning()) {
-            bindService(Intent(this, MusicService::class.java),
-                connection,
-                BIND_AUTO_CREATE)
-        }
-
-        LocalBroadcastManager.getInstance(this).registerReceiver(
-            receiver,
-            IntentFilter("mPlayer")
-        )
-
+        player = MediaPlayer()
         Log.i(TAG, "onStart()")
     }
 
-    override fun onPause() {
-        service?.let {
-            unbindService(connection)
-            service = null
+    /**
+     * Starts the music player playback
+     */
+    fun play() {
+        player?.let {
+            if (it.isPlaying) {
+                return
+            }
+            getFiles().random().apply {
+                try {
+                    val descriptor = assets.openFd(this)
+                    it.setDataSource(
+                        descriptor.fileDescriptor,
+                        descriptor.startOffset,
+                        descriptor.length
+                    )
+                    descriptor.close()
+                    it.prepare()
+                } catch (e: IOException) {
+                    Log.w(TAG, "Could not open file", e)
+                    return
+                }
+                it.isLooping = true
+                it.start()
+
+                // display song info
+                musicInfoTextView?.text = this
+                Log.i(TAG, "Playing song $this")
+            }
         }
-        super.onPause()
-        Log.i(TAG, "onPause()")
     }
 
-    override fun onStop() {
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver)
-        super.onStop()
-        Log.i(TAG, "onStop()")
+    /**
+     * Stops the music player playback
+     */
+    fun stop() {
+        player?.let {
+            if (it.isPlaying) {
+                it.stop()
+            }
+            it.reset()
+        }
+
+        // display song info
+        musicInfoTextView?.text = ""
     }
 
-    @Suppress("DEPRECATION")
-    private fun isServiceRunning(): Boolean =
-        (getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager)
-            .getRunningServices(Int.MAX_VALUE)
-            .any { it.service.className == MusicService::class.java.canonicalName }
+    /**
+     * Returns the list of mp3 files in the assets folder
+     *
+     * @return
+     */
+    private fun getFiles(): List<String> =
+        assets.list("")?.filter { it.toLowerCase(Locale.getDefault()).endsWith("mp3") }
+            ?: emptyList()
 
     companion object {
         private val TAG = MainActivity::class.java.simpleName
