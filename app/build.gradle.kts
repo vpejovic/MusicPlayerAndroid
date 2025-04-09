adde4148f7f6b20e6390aaadbc10d6d844b5bc74
veljko <veljko.pejovic@fri.uni-lj.si>
2025-04-09 14:01:13 +0200

    API 35.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "si.uni_lj.fri.musicplayer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "si.uni_lj.fri.musicplayer"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
